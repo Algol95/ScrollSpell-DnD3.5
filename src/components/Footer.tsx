7227d711a6d42c331d06394fa6644566e3d3acf7
Algol95 <angelaragon.developer@gmail.com>
2026-03-29 13:39:13 +0200

    feat: actualizar enlaces y mejorar contenido en el pie de página

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,25 @@ import { FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
 export function Footer() {
   return (
     <footer className="w-full py-6 mt-10 text-center text-xs text-muted-foreground border-t border-border bg-background/80 flex flex-col items-center gap-2">
-      <span>
+      <p>
+        La página ha sido elaborada sin ánimo de lucro y continúa en fase de
+        desarrollo.
+      </p>
+      <p>
         &copy; {new Date().getFullYear()} Grimorio Arcano. Hecho por{" "}
         <a
-          href="https://www.linkedin.com/in/angel-aragon"
+          href="https://www.linkedin.com/in/angel-dev-aragon"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-gold transition-colors"
+          className="hover:text-gold transition-colors text-accent uppercase"
         >
           Ángel Aragón
         </a>
         . Inspirado en D&D 3.5. No oficial.
-      </span>
+      </p>
       <div className="flex items-center gap-4 justify-center mb-1">
         <a
-          href="https://www.linkedin.com/in/tu-linkedin" // Cambia por tu URL
+          href="https://www.linkedin.com/in/angel-dev-aragon"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gold transition-colors"
@@ -26,7 +30,7 @@ export function Footer() {
           <FaLinkedin className="h-5 w-5" />
         </a>
         <a
-          href="https://github.com/tu-github" // Cambia por tu URL
+          href="https://github.com/Algol95"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gold transition-colors"
@@ -35,10 +39,10 @@ export function Footer() {
           <FaGithub className="h-5 w-5" />
         </a>
         <a
-          href="https://tu-portfolio.com" // Cambia por tu URL
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:text-gold transition-colors"
+          className="hover:text-gold transition-colors hidden"
           aria-label="Portfolio"
         >
           <FaGlobe className="h-5 w-5" />
